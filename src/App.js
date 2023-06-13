08a73af3310a718bd62b2e8b85a2057aaf3d88c4
Lilian Lee <lilianlee0213@gmail.com>
2023-06-13 19:57:59 -0400

    render components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Work from './components/Work';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<Header />
+			<main>
+				<Home />
+				<Work />
+				<About />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
