1add2aea8de2234365ddad3760f62b51b8fe9a7a
Lilian Lee <lilianlee0213@gmail.com>
2023-06-13 20:10:44 -0400

    reset and global styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './styles/reset.css';
+import './styles/globalStyles.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Work from './components/Work';
