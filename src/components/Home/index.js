f1885933a51b727c1e9be5e100a6bfcb6dcff80d
Lilian Lee <lilianlee0213@gmail.com>
2023-06-14 19:35:39 -0400

    fix transforming on scroll

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,17 +8,24 @@ export default function Home() {
 		target: targetRef,
 		offset: ['start end', 'end start'],
 	});
-	const openBox = useTransform(scrollYProgress, [0, 0.3], [-500, 400]);
-	const frameOut = useTransform(scrollYProgress, [0, 0.5], [-250, 300]);
-	const frameRotateX = useTransform(scrollYProgress, [0.5, 0.55], [90, 30]);
-	const frameRotateY = useTransform(scrollYProgress, [0.5, 0.55], [0, -30]);
+	// transform: rotateX(-50deg) rotateY(50deg);
+
+	const rotateXBox = useTransform(scrollYProgress, [0, 0.2], [0, -45]);
+	const rotateYBox = useTransform(scrollYProgress, [0, 0.2], [0, -45]);
+	const openBox = useTransform(scrollYProgress, [0.2, 0.4], [0, 400]);
+	const frameOut = useTransform(scrollYProgress, [0.4, 0.5], [-150, 300]);
+	const frameRotateX = useTransform(scrollYProgress, [0.5, 0.7], [90, 45]);
+	const frameRotateY = useTransform(scrollYProgress, [0.5, 0.7], [0, 55]);
 
 	return (
 		<div className="section-wrapper">
 			<section id="home" className="section section-1" ref={targetRef}>
 				<div className=" h-100 ff-main section-1-container">
 					<div className="sticky cube-container">
-						<div ref={cubeRef} className="cube">
+						<motion.div
+							ref={cubeRef}
+							className="cube"
+							style={{rotateX: rotateXBox, rotateY: rotateYBox}}>
 							<div id="front" className="card">
 								Front
 							</div>
@@ -31,7 +38,7 @@ export default function Home() {
 							<div id="right" className="card">
 								Right
 							</div>
-							<motion.div style={{left: openBox}} id="top" className="card">
+							<motion.div style={{right: openBox}} id="top" className="card">
 								Top
 							</motion.div>
 							<div id="bottom" className="card">
@@ -63,7 +70,7 @@ export default function Home() {
 									<div className="photo-frame-text">My Card4</div>
 								</motion.div>
 							</motion.div>
-						</div>
+						</motion.div>
 					</div>
 				</div>
 			</section>
