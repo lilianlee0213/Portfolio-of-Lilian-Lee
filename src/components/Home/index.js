b63439c86ddc780a8f37e0eb7be61bdcc1d81a8b
Lilian Lee <lilianlee0213@gmail.com>
2023-06-15 17:40:57 -0400

    title animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,37 +3,33 @@ import './style.css';
 import {
 	clamp,
 	motion,
+	useInView,
 	useMotionValueEvent,
 	useScroll,
 	useTransform,
 } from 'framer-motion';
 export default function Home() {
-	const [isOverflowHidden, setIsOverflowHidden] = useState(false);
-	const cubeRef = useRef(null);
 	const targetRef = useRef(null);
+	const titleRef = useRef(null);
+	const isInView = useInView(titleRef, {once: true});
 	const {scrollYProgress} = useScroll({
 		target: targetRef,
 		offset: ['start end', 'end start'],
 	});
-	const rotateXBox = useTransform(scrollYProgress, [0, 0.3], [140, -410]);
-	const rotateYBox = useTransform(scrollYProgress, [0, 0.3], [140, -410]);
-	const openBox = useTransform(scrollYProgress, [0.3, 0.35], [0, 300]);
-	const cardY = useTransform(scrollYProgress, [0.35, 0.4], [70, -350]);
+	const upboxY = useTransform(scrollYProgress, [0.25, 0.3], ['300%', '0%']);
+	const rotateXBox = useTransform(scrollYProgress, [0.3, 0.6], [140, -410]);
+	const rotateYBox = useTransform(scrollYProgress, [0.3, 0.6], [140, -410]);
+	const openBox = useTransform(scrollYProgress, [0.6, 0.65], [0, 300]);
+	const cardY = useTransform(scrollYProgress, [0.65, 0.7], [70, -350]);
 
-	useMotionValueEvent(scrollYProgress, 'change', (latest) => {
-		if (latest > 0.365) {
-			setIsOverflowHidden(true);
-		} else {
-			setIsOverflowHidden(false);
-		}
-	});
 	return (
 		<div className="section-wrapper">
 			<section id="home" className="section section-1" ref={targetRef}>
 				<div className="sticky ff-main hero">
-					<div className="flex justify-center align-center cube-container">
+					<motion.div
+						className="flex justify-center align-center cube-container"
+						style={{y: upboxY}}>
 						<motion.div
-							ref={cubeRef}
 							className="cube"
 							style={{rotateX: rotateXBox, rotateY: rotateYBox}}>
 							<div id="front" className="card">
@@ -61,35 +57,8 @@ export default function Home() {
 								<div className="photo-frame-image"></div>
 								<div className="photo-frame-text">My Card1</div>
 							</motion.div>
-							{/* <motion.div
-								id="photo-cards"
-								className="card"
-								style={{
-									bottom: frameOut,
-									rotateX: frameRotateX,
-									rotateY: frameRotateY,
-									overflow: isOverflowHidden ? 'inherit' : 'hidden',
-								}}>
-								<motion.div className="absolute photo-frame frame-1">
-									<div className="photo-frame-image"></div>
-									<div className="photo-frame-text">My Card1</div>
-								</motion.div>
-
-								<motion.div className="absolute photo-frame frame-2">
-									<div className="photo-frame-image"></div>
-									<div className="photo-frame-text">My Card2</div>
-								</motion.div>
-								<motion.div className="absolute photo-frame frame-3">
-									<div className="photo-frame-image"></div>
-									<div className="photo-frame-text">My Card3</div>
-								</motion.div>
-								<motion.div className="absolute photo-frame frame-4">
-									<div className="photo-frame-image"></div>
-									<div className="photo-frame-text">My Card4</div>
-								</motion.div>
-							</motion.div> */}
 						</motion.div>
-					</div>
+					</motion.div>
 					<div className="absolute w-100 text-marquee">
 						<div className="flex marquee">
 							<p>Creative & Curious</p>
@@ -100,7 +69,14 @@ export default function Home() {
 							<p>Creative & Curious</p>
 						</div>
 					</div>
-					<div className="absolute text-title">
+					<motion.div
+						ref={titleRef}
+						className="absolute text-title"
+						style={{
+							transform: isInView ? 'none' : 'translateX(-200px)',
+							opacity: isInView ? 1 : 0,
+							transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+						}}>
 						<h1 className="ff-main fw-700 uppercase">
 							Front
 							<br />
@@ -108,7 +84,7 @@ export default function Home() {
 							<br />
 							developer
 						</h1>
-					</div>
+					</motion.div>
 				</div>
 			</section>
 			<section id="home" className="section section-home">
