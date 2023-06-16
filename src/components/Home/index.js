568193a5ca0d2e1eca1779a6e793d6a67aa29a88
Lilian Lee <lilianlee0213@gmail.com>
2023-06-16 18:22:50 -0400

    page 2

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,22 @@
 import './style.css';
-
+import {motion} from 'framer-motion';
 import Hero from './Hero';
 
 export default function Home() {
 	return (
 		<div className="section-wrapper">
 			<Hero />
+			<section className="section page-2" style={{}}>
+				<div className="sticky sticky-page-2">
+					<h2 className="absolute fw-600 text-center page2-text">
+						I love creating fun, engaging, and interactive things for the web.
+					</h2>
+					<motion.div className="absolute page2-circle"></motion.div>
+					<h2 className="absolute fw-600 text-center page2-text page2-text-transparent">
+						I love creating fun, engaging, and interactive things for the web.
+					</h2>
+				</div>
+			</section>
 		</div>
 	);
 }
