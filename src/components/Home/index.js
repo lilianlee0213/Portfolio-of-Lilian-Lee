2dc3ad00db1b285d02341e06072a0a75607b5116
Lilian Lee <lilianlee0213@gmail.com>
2023-06-19 12:25:03 -0400

    page2 animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,36 @@
 import './style.css';
-import {motion} from 'framer-motion';
+import {motion, useScroll, useTransform} from 'framer-motion';
 import Hero from './Hero';
+import {useRef} from 'react';
 
 export default function Home() {
+	const targetRef = useRef();
+	const {scrollYProgress} = useScroll({
+		target: targetRef,
+		offset: ['start end', 'end start'],
+	});
+	const circleWidth = useTransform(scrollYProgress, [0.3, 0.8], ['0%', '150%']);
+	const opacity = useTransform(scrollYProgress, [0.85, 1], [1, 0]);
 	return (
 		<div className="section-wrapper">
 			<Hero />
-			<section className="section page-2" style={{}}>
+			<motion.section
+				className="section page-2"
+				ref={targetRef}
+				style={{opacity}}>
 				<div className="sticky sticky-page-2">
 					<h2 className="absolute fw-600 text-center page2-text">
 						I love creating fun, engaging, and interactive things for the web.
 					</h2>
-					<motion.div className="absolute page2-circle"></motion.div>
+					<motion.div
+						className="absolute page2-circle"
+						style={{width: circleWidth}}></motion.div>
 					<h2 className="absolute fw-600 text-center page2-text page2-text-transparent">
 						I love creating fun, engaging, and interactive things for the web.
 					</h2>
+					<p className="absolute page2-title">-Lilian Lee</p>
 				</div>
-			</section>
+			</motion.section>
 		</div>
 	);
 }
