3e054920bec7118f2f22d905dd83bc1a82197383
Lilian Lee <lilianlee0213@gmail.com>
2023-06-20 15:56:43 -0400

    redo hero section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,94 @@
 import './style.css';
-import {motion, useScroll, useTransform} from 'framer-motion';
-import Hero from './Hero';
+import {motion, useInView, useScroll, useTransform} from 'framer-motion';
 import {useRef} from 'react';
 import AnimatedText from '../AnimatedText/animatedText';
 
+const MarqueeSlide = () => {
+	return (
+		<motion.div className="marquee-slide">
+			<span className="marquee-text">I think creative &amp; I build them</span>
+			<span className="marquee-text">I think creative &amp; I build them</span>
+			<span className="marquee-text">I think creative &amp; I build them</span>
+			<span className="marquee-text">I think creative &amp; I build them</span>
+		</motion.div>
+	);
+};
 export default function Home() {
 	const targetRef = useRef(null);
+	const inViewRef = useRef(null);
+	const isInView = useInView(inViewRef, {once: true});
 	const {scrollYProgress} = useScroll({
 		target: targetRef,
 		offset: ['start end', 'end start'],
 	});
 	const circleWidth = useTransform(scrollYProgress, [0.3, 0.8], ['0%', '150%']);
 	const opacity = useTransform(scrollYProgress, [0.85, 1], [1, 0]);
+
 	return (
 		<div className="section-wrapper">
-			<Hero />
+			<section className="section section-hero">
+				<div className="absolute w-100 hero-heading">
+					<div className="absolute w-100 fw-700 uppercase text-center hero-main-text hero-main-top-text">
+						<AnimatedText text="Hello There"></AnimatedText>
+					</div>
+					<motion.div className="absolute flex justify-center align-end hero-main-middle">
+						<motion.h1
+							style={{
+								opacity: isInView ? 1 : 0,
+								transition: 'all .6s cubic-bezier(0.17, 0.55, 0.55, 1) 1.3s',
+							}}>
+							I'm
+						</motion.h1>
+						<motion.div
+							className="hero-main-middle-image"
+							initial={{scale: 1}}
+							animate={{scale: 1}}>
+							<motion.div
+								className="hero-image"
+								whileHover={{scale: 1.1}}
+								transition={{duration: 0.3}}></motion.div>
+						</motion.div>
+						<motion.h1
+							style={{
+								opacity: isInView ? 1 : 0,
+								transition: 'all .6s cubic-bezier(0.17, 0.55, 0.55, 1) 1.3s',
+							}}>
+							Front-end <br />
+							Developer
+						</motion.h1>
+					</motion.div>
+					<div className="absolute w-100 fw-700 uppercase text-center hero-main-text hero-main-top-text text-transparent">
+						<AnimatedText text="Hello There"></AnimatedText>
+					</div>
+					<div className="absolute w-100 fw-700 uppercase text-center hero-main-text hero-main-bottom-text">
+						<AnimatedText text="Lilian Lee"></AnimatedText>
+					</div>
+				</div>
+				<motion.div
+					ref={inViewRef}
+					className="absolute w-100 marquee-container marquee-1"
+					style={{
+						opacity: isInView ? 1 : 0,
+						transition: 'all .6s cubic-bezier(0.17, 0.55, 0.55, 1) 1.3s',
+					}}>
+					<>
+						{MarqueeSlide()}
+						{MarqueeSlide()}
+					</>
+				</motion.div>
+				<motion.div
+					ref={inViewRef}
+					className="absolute w-100 marquee-container marquee-2"
+					style={{
+						opacity: isInView ? 1 : 0,
+						transition: 'all .6s cubic-bezier(0.17, 0.55, 0.55, 1) 1.3s',
+					}}>
+					<>
+						{MarqueeSlide()}
+						{MarqueeSlide()}
+					</>
+				</motion.div>
+			</section>
 			<motion.section
 				className="section slide-one"
 				ref={targetRef}
