16426f9a4195c375f94d05bd7a17cd6538715d2c
Lilian Lee <lilianlee0213@gmail.com>
2023-06-21 14:58:48 -0400

    stories imges as a seperate file

---
+++ src/components/About/stories.js
@@ -0,0 +1,24 @@
+import teaching1 from '../../images/stories/teaching1.jpg';
+import teaching2 from '../../images/stories/teaching2.jpg';
+import teaching3 from '../../images/stories/teaching3.jpg';
+import teaching4 from '../../images/stories/teaching4.jpg';
+import goodTimes1 from '../../images/stories/goodTimes1.jpg';
+import goodTimes2 from '../../images/stories/goodTimes2.jpg';
+import goodTimes3 from '../../images/stories/goodTimes3.jpg';
+import goodTimes4 from '../../images/stories/goodTimes4.jpg';
+import goodTimes5 from '../../images/stories/goodTimes5.jpg';
+import vibes1 from '../../images/stories/vibes1.jpg';
+import vibes2 from '../../images/stories/vibes2.jpg';
+import vibes3 from '../../images/stories/vibes3.jpg';
+import vibes4 from '../../images/stories/vibes4.jpg';
+import vibes5 from '../../images/stories/vibes5.jpg';
+
+export const teachingImg = [teaching1, teaching2, teaching3, teaching4];
+export const goodTimesImg = [
+	goodTimes1,
+	goodTimes2,
+	goodTimes3,
+	goodTimes4,
+	goodTimes5,
+];
+export const vibesImg = [vibes1, vibes2, vibes3, vibes4, vibes5];
